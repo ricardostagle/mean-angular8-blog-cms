0f58f885a237153b853c5d3a9437582ea62e2525
Kain <ricardo.stagle@gmail.com>
2020-06-13 14:11:05 -0500

    fix header bug

--- client/src/app/footer/footer.component.ts
+++ client/src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public href: string = "";
+  route: string;
+  currentURL='';
+  constructor(private router: Router) { 
+    this.currentURL = window.location.href; 
+    if(this.currentURL.indexOf("#") !== -1){
+      var strArray = this.currentURL.split("#");
+      this.currentURL = strArray[0];
+      
+    console.log(this.currentURL);
+    }
   }
 
+  ngOnInit(): void {
+    this.href = this.router.url;
+  }
 }
+
--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -11,9 +12,21 @@ export class NavbarComponent implements OnInit {
 
   sticky: boolean = false;
   elementPosition: any;
-
-  constructor( ) {
-
+  pagesflag = 0;
+  currentURL='';
+  hostname='';
+  epos=0;
+    
+  constructor(private router: Router) { 
+    var url = window.location.href;
+    var arr = url.split("/");
+    this.hostname = arr[0] + "//" + arr[2]; 
+    this.currentURL = window.location.href; 
+    if(this.currentURL.indexOf("/home") !== -1 
+        || this.currentURL.indexOf("/es-es") !== -1
+        || this.hostname == this.currentURL){
+          this.hostname = this.currentURL;
+    }
   }
 
   ngOnInit() {
@@ -26,15 +39,33 @@ export class NavbarComponent implements OnInit {
 
   @HostListener('window:scroll', ['$event'])
     handleScroll(){
+
+      this.pagesflag = 0;
+      this.currentURL = window.location.href; 
+
+      if(this.currentURL.indexOf("/home") !== -1 
+        || this.currentURL.indexOf("/es-es") !== -1){
+          this.pagesflag = 1;
+      }
+
       const windowScroll = window.pageYOffset;
       let element = document.querySelector('.navbar');
-      element.style.display = "none";
-      if(windowScroll >= (this.elementPosition + 150)){
-        this.sticky = true;
-        /*element.style.display = "flex";*/
-      } else {
+
+      if(this.pagesflag == 1){
+        this.epos = (this.pagesflag) ? this.elementPosition + 70 : this.elementPosition;
+        if (element instanceof HTMLElement) {
+          if(windowScroll >= this.epos){
+            this.sticky = true;
+            element.style.marginTop = "0";
+          } else {
+            this.sticky = false;
+            element.style.marginTop = "-62px";
+          }
+        } else {
+          throw new Error("element #test not in document")
+        }
+      }else{
         this.sticky = false;
-        
       }
     }
 
